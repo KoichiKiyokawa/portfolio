13a0de7fda216a9b643bd843b531d9c7b0c1589c
K.Kiyokawa <koichi20110068@gmail.com>
2020-01-31 23:09:57 +0900

    feat: add description to About Me

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -3,6 +3,7 @@ div(data-aos="fade-up" data-aos-delay="500")
   h1.title About Me
   .body
     p 1998年生まれ。
+    p エンジニアに憧れる学生。
     p 高校生1年生のとき、先輩に誘われて入った数学部に入部。
     p そこで、数学の研究は全くせずにプログラミングを学び、今にいたる。
     br
