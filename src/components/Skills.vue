516bfc5860f3c971e79026f0fcb46144b19506db
K.Kiyokawa <koichi20110068@gmail.com>
2020-02-01 01:45:24 +0900

    refactor: split data and view

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -3,62 +3,91 @@ div#skills(data-aos="fade-up" data-aos-delay="500")
   h1.title Skills
   h2.subtitle 言語, フレームワーク, etc... (経歴が長い順に並んでいます)
   .body.grid
-    .each-skill
-      img(src="https://y-hilite.com/wp-content/uploads/2018/02/rails_logo.png" alt="Rails")
-      p Rails: 2018/4~
-      p ER図通りにMigrationファイルやモデルファイルを書くことができます。たぶん。
-      p Rspecでテストケースを書いたりも。
-      p テンプレートエンジンはhamlとslimを扱ったとこがあります。
-      p 最近はapiサーバーとしてしか使っていないので、helperとか忘れ気味...
-    .each-skil
-      img(src="https://classmethod.jp/wp-content/uploads/2017/03/partner-logo-githubenterprise.png", alt="GitHub")
-      p GitHub
-      p Gitの操作、GitHub Flowでの開発ができます。機能ごとにブランチを切って、プルリクを作ったり、レビューをしたり。こまめにコミットしておいてから、git rebase -iでコミットをまとめる派。
-    .each-skill
-      img(src="https://jp.vuejs.org/images/logo.png" alt="Vue")
-      p Vue: 2018/11~
-      p Vuex, Vue routerも。色々よしなにやってくれるNuxtが好き。このサイトもNuxtで作っています(適当に設定していたらPWAに対応してた...)。
-      p TypeScript + クラススタイルの書き方が好きですが、VuexとTypeScriptの組み合わせはまだ勉強中...
-    .each-skill
-      img(src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png" alt="React")
-      p React
-      p Hooksを使った書き方のほうが経験が長いです
-      p TypeScript, Next, Redux, StyledComponent, React Nativeも体験しました
-    .each-skill
-      img(src="https://josys-navi.hiblead.co.jp/wp-content/uploads/2019/03/Logo_Java.jpg" alt ="Java")
-      p Java
-      p 大学の卒業研究でJavaを書いていました
-      p 他にもJavaFXでデスクトップアプリを作ったり
-    .each-skill
-      img(src="https://qiita-user-contents.imgix.net/https%3A%2F%2Fwww.creationline.com%2Fwp-content%2Fuploads%2F2018%2F06%2FMoby2018.png?ixlib=rb-1.2.2&auto=format&gif-q=60&q=75&s=98741ddc33689c0fba6dc2e4c6838d9e" alt="Docker")
-      p Docker
-      p Dockerfile, docker-compose.ymlが書けます。
-      p とりあえずalpineをベースイメージにしておけば、イメージサイズが小さくできて何かと便利だと思っている。
-    .each-skill
-      img(src="https://cdn-ak.f.st-hatena.com/images/fotolife/t/tikasan0804/20170719/20170719172956.png" alt="Golang")
-      p Go: 2018/12~
-      p EchoでかんたんなAPIサーバーを作ったことがあります
-      p まだまだ勉強途中...
-      p Clean Architectureも勉強して、
-      p Go + Clean Architectureでサーバーサイドを書いてみたい
-    .each-skill
-      img(src="https://teratail-v2.storage.googleapis.com/uploads/contributed_images/98878d582fec33e5c3dfe95931b8d8f1.png" alt="Firebase")
-      p Firebase
-      p Authentification, FireStoreが使えます。ML Kitとかも使ってみたい。
-    .each-skill
-      img(src="https://raw.githubusercontent.com/neovim/neovim.github.io/master/logos/neovim-logo-300x87.png" alt="Vim")
-      p Vim(技術なのか...?)
-      p Vimを普段遣いしています。
-      p こだわりの設定は
-       |
-       a(href="https://github.com/KoichiKiyokawa/dotfiles/blob/master/nvim/init.vim" target="_blank") 
-         | こちらに
-         i.fab.fa-github
-      p SSHでサーバーに繋いでいるときに音速でファイル編集ができます
-    // TODO: AWS EC2, Lambda, Neptune
-    .each-skil
-      img()
+    .each-skill(v-for="(skill, i) in skills" :key="i")
+      img(:src="skill.imgSrc" :alt="skill.imgAlt || skill.name")
+      p {{skill.name}}{{skill.since ? `: ${skill.since}~` : ''}}
+      p(v-for="(desc, i) in skill.description.split('|')" :key="i" v-html="desc")
 </template>
+
+<script>
+export default {
+  data: () => ({
+    /**
+     * @type skill {
+     *   name: string
+     *   since?: string
+     *   imgSrc: string
+     *   imgAlt?: string
+     *   description: string
+     * }
+     * To break line in description, you should put `|`.
+     */
+
+    // @type skill[]
+    skills: [
+      {
+        name: 'Rails',
+        since: '2018/4',
+        imgSrc: 'https://y-hilite.com/wp-content/uploads/2018/02/rails_logo.png',
+        description:
+          'ER図通りにMigrationファイルやModelファイルを書くことができます。たぶん。|Rspecでテストケースを書いたりも。|テンプレートエンジンはhamlとslimを扱ったとこがあります。|最近はapiサーバーとしてしか使っていないので、helperとか忘れ気味...'
+      },
+      {
+        name: 'GitHub',
+        imgSrc: 'https://classmethod.jp/wp-content/uploads/2017/03/partner-logo-githubenterprise.png',
+        description:
+          'Gitの操作、GitHub Flowでの開発ができます。機能ごとにブランチを切って、プルリクを作ったり、レビューをしたり。|こまめにコミットしておいてから、git rebase -iでコミットをまとめる派。'
+      },
+      {
+        name: 'Vue',
+        since: '2018/11',
+        imgSrc: 'https://jp.vuejs.org/images/logo.png',
+        description:
+          'Vuex, Vue routerも。色々よしなにやってくれるNuxtが好き。このサイトもNuxtで作っています(適当に設定していたらPWAに対応してた...)。|TypeScript + クラススタイルの書き方が好きですが、VuexとTypeScriptの組み合わせはまだ勉強中...'
+      },
+      {
+        name: 'React',
+        imgSrc: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png',
+        description:
+          'Hooksを使った書き方のほうが経験が長いです|TypeScript, Next, Redux, StyledComponent, React Nativeも体験しました'
+      },
+      {
+        name: 'Java',
+        imgSrc: 'https://josys-navi.hiblead.co.jp/wp-content/uploads/2019/03/Logo_Java.jpg',
+        description: '大学の卒業研究でJavaを書いていました|他にもJavaFXでデスクトップアプリを作ったり'
+      },
+      {
+        name: 'Docker',
+        imgSrc:
+          'https://qiita-user-contents.imgix.net/https%3A%2F%2Fwww.creationline.com%2Fwp-content%2Fuploads%2F2018%2F06%2FMoby2018.png?ixlib=rb-1.2.2&auto=format&gif-q=60&q=75&s=98741ddc33689c0fba6dc2e4c6838d9e',
+        description:
+          'Dockerfile,docker-compose.ymlが書けます。 |とりあえずalpineをベースイメージにしておけば、イメージサイズが小さくできて何かと便利だと思っている。'
+      },
+      {
+        name: 'Go',
+        since: '2018/12',
+        imgSrc: 'https://cdn-ak.f.st-hatena.com/images/fotolife/t/tikasan0804/20170719/20170719172956.png',
+        description:
+          'EchoでかんたんなAPIサーバーを作ったことがあります|まだまだ勉強途中...|Clean Architectureも勉強して、|Go + Clean Architectureでサーバーサイドを書いてみたい'
+      },
+      {
+        name: 'Firebase',
+        imgSrc:
+          'https://teratail-v2.storage.googleapis.com/uploads/contributed_images/98878d582fec33e5c3dfe95931b8d8f1.png',
+        description: 'Authentification, FireStoreが使えます。ML Kitとかも使ってみたい。'
+      },
+      {
+        name: 'Vim(技術なのか...?)',
+        imgSrc: 'https://raw.githubusercontent.com/neovim/neovim.github.io/master/logos/neovim-logo-300x87.png',
+        imgAlt: 'Vim',
+        description:
+          'Vimを普段遣いしています。SSHでサーバーに繋いでいるときに音速でファイル編集ができます|こだわりの設定は<a href="https://github.com/KoichiKiyokawa/dotfiles/blob/master/nvim/init.vim" target="_blank">こちらに<i class="fab fa-github" /></a>'
+      }
+      // TODO: AWS EC2, Lambda, Neptune
+    ]
+  })
+}
+</script>
 
 <style lang="sass" scoped>
 #skills
