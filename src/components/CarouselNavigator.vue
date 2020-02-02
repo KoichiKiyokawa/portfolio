0b37220db808395ccbae7939e9bdde8762c98cd3
K.Kiyokawa <koichi20110068@gmail.com>
2020-02-02 19:26:23 +0900

    feat: modify Dormio carousel

---
+++ src/components/CarouselNavigator.vue
@@ -0,0 +1,17 @@
+<template lang="pug">
+.carousel-navigator
+  button(@click="$emit('prev-page')")
+    i.fas.fa-arrow-left
+  button(@click="$emit('next-page')")
+    i.fas.fa-arrow-right
+</template>
+
+<style lang="sass">
+.carousel-navigator
+  display: flex
+  justify-content: space-between
+  button
+    border-radius: 50%
+    font-size: 2em
+    background-color: #f2f2f2
+</style>
--- src/components/Works.vue
+++ src/components/Works.vue
@@ -1,9 +1,10 @@
 <template lang="pug">
 div(data-aos="fade-up" data-aos-delay="500")
   h1.title Works
-  carousel(:per-page="1" navigation-enabled)
+  carousel(:per-page="1" :navigate-to="page")
     slide
-      img.work-img(src="https://user-images.githubusercontent.com/40315079/73530134-3f201000-445b-11ea-91db-eff88d704cc4.gif")
+      video.work-img(src="dormio_summary.mp4" autoplay loop)
+      carousel-navigator(@prev-page="turnPrevPage" @next-page="turnNextPage")
       p.subtitle Dormio
       .body
         p 寮で暮らす人と管理人をつなぐアプリ
@@ -15,6 +16,7 @@ div(data-aos="fade-up" data-aos-delay="500")
             i.fab.fa-github
     slide
       img.work-img(src="https://user-images.githubusercontent.com/40315079/73604795-dca14e00-45d9-11ea-8240-efaa58e6ba2e.gif")
+      carousel-navigator(@prev-page="turnPrevPage" @next-page="turnNextPage")
       p.subtitle Money Manager
       .body
         p Java製のGUI家計簿アプリ
@@ -28,12 +30,33 @@ div(data-aos="fade-up" data-aos-delay="500")
 
 <script>
 import { Carousel, Slide } from 'vue-carousel'
+import CarouselNavigator from '~/components/CarouselNavigator'
+
+const CAROUSEL_MAX_PAGE = 1 // page: 0, 1
 
 export default {
-  components: { Carousel, Slide }
+  components: { Carousel, Slide, CarouselNavigator },
+  data: () => ({ page: 0 }),
+  methods: {
+    turnPrevPage() {
+      if (this.page === 0) {
+        this.page = CAROUSEL_MAX_PAGE
+      } else {
+        this.page--
+      }
+    },
+    turnNextPage() {
+      if (this.page === CAROUSEL_MAX_PAGE) {
+        this.page = 0
+      } else {
+        this.page++
+      }
+    }
+  }
 }
 </script>
 <style lang="sass">
 .work-img
   height: 20em
+  max-width: 90vw
 </style>
