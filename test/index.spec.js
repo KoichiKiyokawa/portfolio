a9141fa7a24fc4679f065e989bef004cc1792486
K.Kiyokawa <koichi20110068@gmail.com>
2020-02-13 16:10:20 +0900

    refactor: rename imported component

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,13 +1,13 @@
 import { mount } from '@vue/test-utils'
 import Vue from 'vue'
 import VueScrollTo from 'vue-scrollto'
-import index from '~/pages/index'
+import IndexPage from '~/pages/index'
 
 Vue.use(VueScrollTo)
 
 describe('index', () => {
   test('is a Vue instance', () => {
-    const wrapper = mount(index)
+    const wrapper = mount(IndexPage)
     expect(wrapper.isVueInstance()).toBeTruthy()
   })
 })
