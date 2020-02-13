424627d8942938ece4ffbf273f35d20cdf0583b7
K.Kiyokawa <koichi20110068@gmail.com>
2020-02-13 15:32:11 +0900

    feat: highlight showing menu

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,25 +1,36 @@
 <template lang="pug">
 .wrapper
-  img.shrink(src="icon.png" alt="logo")
+  Intersect(@enter="showingMenu = null" :threshold="[0]")
+    img.shrink(src="icon.png" alt="logo")
   h1.title K.Kiyokawa
   h2.subtitle My portfolio
   .sticky-header
-    .clickable.menu-item(v-scroll-to="'#about-me'") About Me
-    .clickable.menu-item(v-scroll-to="'#skills'") Skills
-    .clickable.menu-item(v-scroll-to="'#works'") Works
+    .clickable.menu-item(v-scroll-to="'#about-me'" @click="showingMenu = 'about-me'" :class="{selected: showingMenu === 'about-me'}") About Me
+    .clickable.menu-item(v-scroll-to="'#skills'" @click="showingMenu = 'skills'" :class="{selected: showingMenu === 'skills'}") Skills
+    .clickable.menu-item(v-scroll-to="'#works'" @click="showingMenu = 'works'" :class="{selected: showingMenu === 'works'}") Works
     a.clickable.menu-item(href="https://github.com/KoichiKiyokawa" target="_blank")
       i.fab.fa-github.mr-small
       | GitHub
+
   .mt-large
-  AboutMe#about-me.shrink
+
+  Intersect(@enter="showingMenu = 'about-me'" :threshold="[0]")
+    AboutMe#about-me.shrink
+
   .mt-middle
-  Skills#skills.shrink
+
+  Intersect(@enter="showingMenu = 'skills'" :threshold="[0]")
+    Skills#skills.shrink
+
   .mt-middle
-  Works#works.shrink
+
+  Intersect(@enter="showingMenu = 'works'" :threshold="[0]")
+    Works#works.shrink
   ScrollTop
 </template>
 
 <script>
+import Intersect from 'vue-intersect'
 import ScrollTop from '~/components/ScrollTop'
 import AboutMe from '~/components/AboutMe'
 import Skills from '~/components/Skills'
@@ -27,10 +38,14 @@ import Works from '~/components/Works'
 
 export default {
   components: {
+    Intersect,
     ScrollTop,
     AboutMe,
     Skills,
     Works
-  }
+  },
+  data: () => ({
+    showingMenu: ''
+  })
 }
 </script>
